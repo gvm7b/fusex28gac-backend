0fda527f48e9cfe83340fd55ea7fc45908a44ab5
unknown <guivict233@gmail.com>
2026-04-09 15:16:43 -0300

    feat: estrutura inicial Spring Boot

---
+++ src/main/java/br/com/fusex28gac/fusex_backend/FusexBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.fusex28gac.fusex_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FusexBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FusexBackendApplication.class, args);
+	}
+
+}
